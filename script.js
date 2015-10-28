44b8e752bd2061b1c1263a52712b0c5f8ee5a193
Nick Portokallidis <portokallidis@gmail.com>
2015-10-28 18:45:59 +0200

    update authentication with devices

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-
 angular.module('CarreExample', ['ngCookies'])
-.config(function($locationProvider) {
-  $locationProvider.html5Mode(true);
-})
-.controller('ExampleController', function($scope,$cookies,$location) {
-  
-  //set up the urls 
-  var CARRE_DEVICES='http://devices.carre-project.eu/devices/accounts';
-  var baseUrl=$location.absUrl();
-  $scope.loginUrl=CARRE_DEVICES+'/login?next='+baseUrl+'?login'; //pass the login as a parameter to catch later
-  $scope.logoutUrl=CARRE_DEVICES+'/logout?next='+baseUrl+'?logout'; //same for the logout
-  
-  console.log('LoginURL: ',$scope.loginUrl);
-  console.log('LogoutURL: ',$scope.logoutUrl);
-  
-  
-  // Retrieving a cookie and set initial user object
-  $scope.user = $scope.cookie = $cookies.getObject('CARRE_USER');
-  console.log($cookies.get('CARRE_USER'));
-  
-  // Retrieving url params
-  var params = $location.search();
+  .config(function($locationProvider) {
+    $locationProvider.html5Mode(true);
+  })
+  .controller('ExampleController', function($scope, $cookies, $location,$http) {
 
-  //check for cookie or url get parameters
-  if(params.login && params.username){
-    delete params.login; //delete the extra param we put before
-    $scope.user=$scope.cookie=params; //set user object
-    $cookies.putObject('CARRE_USER',$scope.cookie,{'domain': '.carre-project.eu','path':'/'}); //set browser cookie
-  } else if(params.logout){
-    $scope.user=$scope.cookie=null; //remove user object
-    $cookies.remove('CARRE_USER',{'domain': '.carre-project.eu','path':'/'});  //remove browser cookie
-  }
-  
-  //clean up the browser url
-  $location.url('/').replace();
-});
+    //set up the urls 
+    var CARRE_DEVICES = 'http://devices.carre-project.eu/devices/accounts';
+    var API = 'http://beta.carre-project.eu:5050/carre.kmi.open.ac.uk:443/ws/'; // http://carre.kmi.open.ac.uk:443/ws/
+    
+    
+    //clean up the browser url
+    $location.url('/').replace();
+    var baseUrl = $location.absUrl();
+    
+    $scope.loginUrl = CARRE_DEVICES + '/login?next=' + baseUrl;
+    $scope.logoutUrl = CARRE_DEVICES + '/logout?next=' + baseUrl;
+
+    // Retrieving a cookie and set initial user object
+    var TOKEN = $cookies.get('CARRE_USER') || '';
+
+    //validate cookie token with userProfile api function and get username userGraph
+    if (TOKEN.length > 0) {
+      $http.get(API + 'userProfile?token=' + TOKEN).then(function(res) {
+        $scope.user = {
+          oauth_token: TOKEN,
+          username: res.data.username,
+          email: res.data.email
+        };
+      }, function(err) {
+        $scope.user = {};
+        console.log(err);
+      });
+    }
+    else {
+      $scope.user = {};
+      TOKEN = '';
+    }
+  });
